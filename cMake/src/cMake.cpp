f98fd6648482bf5163dfa864ab5329714dc9d606
Cleenewerck Noé <150229480+CLE4N-HS@users.noreply.github.com>
2025-02-04 08:57:33 +0100

    cMake

---
+++ cMake/src/cMake.cpp
@@ -0,0 +1,21 @@
+﻿// cMake.cpp : définit le point d'entrée de l'application.
+//
+
+#include "cMake.h"
+#include "SFML/Graphics.hpp"
+
+using namespace std;
+
+
+int main()
+{
+	float delta = 0.0f, sPosx = 0.0f, ePosx = 10.0f;
+
+	sf::Vector2f a(0.f, 1.f);
+
+	cout << a.y << endl;
+
+	cout << "Hello CMake." << endl;
+	system("pause");
+	return EXIT_SUCCESS;
+}
